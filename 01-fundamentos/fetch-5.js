0bfb096a9e420e7c3869792f5ad159732c4a50f2
Eskeron <alejandro.escalante@alumno.buap.mx>
2021-04-21 10:06:45 -0500

    fundamentals practice completed

--- 01-fundamentos/fetch-5.js
+++ 01-fundamentos/fetch-5.js
@@ -1,6 +1,14 @@
-fetch('https://reqres.in/api/users/1')
+fetch('https://reqres.in/api/users/10')
 .then(resp => {
-
+    console.log(resp);
+    if (resp.ok){
+        return resp.json()
+    }
+    else{
+        //console.log('no hay no existe')
+        throw new Error('No hay no existe')
+    }
+/*
     resp.clone().json()
     .then(usuario => {
         console.log(usuario.data)
@@ -12,5 +20,10 @@ fetch('https://reqres.in/api/users/1')
     
     resp.json().then(usuario => {
         console.log(usuario.data)
-    });
+    });*/
+})
+.then(console.log)
+.catch(error=>{
+    console.log('error en la petición');
+    console.log(error);
 });
